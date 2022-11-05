6263daae2cd601dacd25c44e405a2765c8c7b6b6
WayneChen2021 <91292224+WayneChen2021@users.noreply.github.com>
2022-11-04 23:59:42 -0400

    Update price.py

--- price.py
+++ price.py
@@ -1,12 +1,14 @@
 class Price(dict):
-    def __init__(self, price:dict = {}, **kwargs) -> None:
+    def __init__(self, price: dict = {}, **kwargs):
+        """
+        Dictionary like object indicating token prices for 1 batch of inputtxs:
+        {
+            "Token 1": 100,
+            "Token 2": 50
+        }
+
+        Parameters:
+        prices: dictionary mapping token types to prices
+        """
         super(self).__init__(price)
         self.update(**kwargs)
-
-        for key, value in self.items():
-            if not isinstance(key, str) and isinstance(value, float):
-                raise TypeError(f'Cannot read price {value} or token type {key}')
-            elif value < 0:
-                raise ValueError(f'Cannot accept negative price {value} of type {key}')
-
-        
