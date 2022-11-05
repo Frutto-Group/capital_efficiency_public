98f19a39f5714cc45ddb659e4a71f06cabf757d2
WayneChen2021 <91292224+WayneChen2021@users.noreply.github.com>
2022-11-04 23:59:26 -0400

    Update poolstatus.py

--- poolstatus.py
+++ poolstatus.py
@@ -1,74 +1,85 @@
+from typing import List, Dict, Tuple, float
 from price import Price
-from typing import List, Tuple
 
 class PoolStatusInterface:
-    def get_total_value(self, price: Price):
+    def get_total_value(self, price: Price) -> float:
+        """
+        Get the value (in dollars) of the pool
+
+        Parameters:
+        price: token prices
+        """
         raise NotImplementedError
 
 
-class MultiTokenPoolStatus(PoolStatusInterface, dict):
-    def __init__(self, status: dict):
-        dict.__init__(self, status)
-        for key, value in self.items():
-            if not isinstance(key, str) and isinstance(value[0], float):
-                raise TypeError(f'Cannot read token amount {value[0]} or token type {key}')
-            elif value[0] < 0:
-                raise ValueError(f'Cannot accept negative token amount {value[0]} of type {key}')
-            elif value[1] <= 0 or value[1] >= 1:
-                raise ValueError(f'Cannot accept k value of {value[0]} of type {key}')
+class MultiTokenPoolStatus(PoolStatusInterface):
+    def __init__(self, status: Dict[str, Tuple[float, float]]):
+        """
+        Represents pool status for multi token liquidity pool market makers
 
-    def get_total_value(self, price: Price):
+        Parameters:
+        status: dictionary indicating tokens' counts and k values; of the form:
+        {
+            "Token a": [100, 0.5]
+        }
+        """
+        dict.__init__(self, status)
+
+    def get_total_value(self, price: Price) -> float:
+        """
+        Get the value (in dollars) of the pool
+
+        Parameters:
+        price: token prices
+        """
         total_value = 0.
         for key, value in self.items():
-            if key not in price:
-                raise KeyError(f'Price of {key} not defined')
-
             total_value += value[0] * price[key]
         
         return total_value
 
 
 class PairwiseTokenPoolStatus(PoolStatusInterface, dict):
-    def __init__(self, token_pairs: List[Tuple[str]], token_amounts: List[Tuple[float]]):
-        for (tokenA, tokenB), (amountA, amountB) in zip(token_pairs, token_amounts):
-            self[(tokenA, tokenB)] = (amountA, amountB)
-            self[(tokenB, tokenA)] = (amountB, amountA)
+    def __init__(self, token_pairs: List[Tuple[str, str]],
+    token_infos: List[Tuple[float, float, float]]):
+        """
+        Represents pool status for 2 token liquidity pool market makers
 
-    def get_total_value(self, price: Price):
+        Parameters:
+        token_pairs: tuples of trading pairs, i.e "['BTC. 'ETH']"; should not 
+        have redundant pairs; of the form:
+        [
+        ["BTC", "ETH"],
+        ["ETH", "BTC"],
+        ["BTC", "USDT"],
+        ["USDT", "BTC"],
+        ]
+        token_infos: token counts and k values for each trading pair; of the form:
+        [
+        [1100, 500, 0],
+        [500, 1100, 0],
+        [200, 500, 0],
+        [500, 200, 0],
+        ]
+        """
+        for (tokenA, tokenB), (amountA, amountB, k) in zip(token_pairs, token_infos):
+            self[(tokenA, tokenB)] = [amountA, amountB, k]
+            self[(tokenB, tokenA)] = [amountB, amountA, k]
+
+    def get_total_value(self, price: Price) -> float:
+        """
+        Get the value (in dollars) of the pool
+
+        Parameters:
+        price: token prices
+        """
         visited = set()
         total_value = 0.
         for (ka, kb), (va, vb) in self.items():
             if (ka, kb) in visited or (kb, ka) in visited:
                 continue
 
-            if ka not in price:
-                raise KeyError(f'Price of {ka} not defined')
-            if kb not in price:
-                raise KeyError(f'Price of {kb} not defined')
-
             visited.add((ka, kb))
             total_value += va * price[ka] + vb * price[kb]
         
-        return total_value
-    
-class PMMPoolStatus(PoolStatusInterface, dict):
-    def __init__(self, token_infos: dict):
-        for k, v in token_infos.items():
-            self[k] = v
-
-    def get_total_value(self, price: Price):
-        visited = set()
-        total_value = 0.
-        for (ka, kb), (va, vb, k) in self.items():
-            if (ka, kb) in visited or (kb, ka) in visited:
-                continue
-
-            if ka not in price:
-                raise KeyError(f'Price of {ka} not defined')
-            if kb not in price:
-                raise KeyError(f'Price of {kb} not defined')
-
-            visited.add((ka, kb))
-            total_value += va * price[ka] + vb * price[kb]
-
         return total_value
