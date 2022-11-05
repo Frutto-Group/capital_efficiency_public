d2ea2eda4a3980964a3c2e760fdae9fd24413e3b
WayneChen2021 <91292224+WayneChen2021@users.noreply.github.com>
2022-11-04 23:55:04 -0400

    Update mcsmm.py

--- mcsmm.py
+++ mcsmm.py
@@ -2,111 +2,50 @@ from typing import List, Tuple
 from imarketmaker import MarketMakerInterface
 from inputtx import InputTx
 from outputtx import OutputTx
-from price import Price
-from poolstatus import PoolStatusInterface, MultiTokenPoolStatus
-from copy import deepcopy
-
+from poolstatus import MultiTokenPoolStatus
 
 class MCSMM(MarketMakerInterface):
-    def __init__(self, tokens: List[str], token_info: List[float], reset_batch: str = "False",
-                 reset_tx: str = "False"):
+    def __init__(self, tokens: List[str], token_infos: List[Tuple[float, float]]):
         """
-        tokens of the form: ["BTC", "ETH", "USDT"]
+        Creates a multi token constant sum liquidity pool market maker
 
-        token_info of the form: [[1100, 0], [2000, 0], [1000, 0]]
-
-        reset_batch: True if reset CSMM to initial state after every batch
-
-        reset_tx: True if reset CSMM to initial state after every InputTx
+        Parameters:
+        1. tokens: specifies tokens in liquidity pool; of the form:
+        ["BTC", "ETH", "USDT"]
+        2. token_infos: specifies starting token balances; of the form:
+        [[1100, 0], [2000, 0], [1000,0]]
         """
-        self.balances = {tokens[i]: token_info[i][0] for i in range(len(tokens))}
+        self.token_info = MultiTokenPoolStatus({tokens[i]: [token_infos[i][0], token_infos[i][1]] \
+            for i in range(len(tokens))})
+        self.equilibriums = None
 
-        self.reset_batch = reset_batch == "True"
-        self.reset_tx = reset_tx == "True"
+    def arbitrage(self, lim: float = 1e-8) -> Tuple[None, None]:
+        """
+        Performs no arbitrage since it is impossible on MCSMM
+        """
+        return [], []
 
-    def configure_arbitrage(self, arb_period: int, arb_actions: int):
-        # there is no arbitrage
-        pass
+    def swap(self, tx: InputTx, out_amt: float = None) -> Tuple[OutputTx, MultiTokenPoolStatus]:
+        """
+        Initiate a swap specified by tx
 
-    def swap(self, tx: InputTx):
-        intype = tx.intype
-        outtype = tx.outtype
-        d = tx.inval
-        p = self.prices[outtype] / self.prices[intype]
+        Parameters:
+        1. tx: transaction
+        2. out_amt: specifies the amount of output token removed
 
-        input_balance_0 = self.balances[intype]
-        output_balance_0 = self.balances[outtype]
+        Returns:
+        1. output information associated with swap (after_rate is incorrect)
+        2. status of pool ater swap
+        """
+        p = self.prices[tx.outtype] / self.prices[tx.intype]
+        if out_amt == None:
+            if (tx.inval / p > self.token_info[tx.outtype]):
+                out_amt = 0
+                tx.inval = 0
+            else:
+                out_amt = tx.inval / p
 
-        # Balance cannot go negative
-        if (d / p > output_balance_0):
-            # refuse transaction if amt drains balance
-            amt = 0
-            d = 0
-        else:
-            amt = d / p
+        output_tx, pool_stat = super().swap(tx, out_amt)
+        output_tx.after_rate = p
 
-        self.balances[intype] += d
-        self.balances[outtype] -= amt
-
-        return {"amt": amt,
-                "market_rate": p,
-                "swap_rate": p,
-                "input_balance_0": input_balance_0,
-                "output_balance_0": output_balance_0,
-                "input_balance_1": self.balances[intype], #enabled
-                "output_balance_1": self.balances[outtype] #enabled
-                }
-
-    def simulate_traffic(self,
-                         traffic: List[List[InputTx]],
-                         external_price: List[Price]
-                         ) -> Tuple[List[List[OutputTx]], List[List[PoolStatusInterface]], PoolStatusInterface, PoolStatusInterface, List[float], List[float]]:
-        outputs = []
-        status = []
-        rates = []
-
-        pool_dict = {}
-        for t in self.balances:
-            pool_dict[t] = (self.balances[t], 0.5)
-        initial_copy = MultiTokenPoolStatus(deepcopy(pool_dict))
-
-        if self.reset_batch or self.reset_tx:
-            balance_copy = deepcopy(self.balances)
-
-        for j, batch in enumerate(traffic):
-            self.prices = external_price[j]
-
-            batch_output = []
-            batch_status = []
-
-            if self.reset_batch or self.reset_tx:
-                self.balances = balance_copy
-                balance_copy = deepcopy(self.balances)
-
-            for c, tx in enumerate(batch):
-                info = self.swap(tx)
-                rates.append(info["swap_rate"])
-
-                batch_output.append(OutputTx(tx.index,
-                                            tx.intype, tx.outtype,
-                                            tx.inval, info["amt"],
-                                            info["input_balance_0"], info["output_balance_0"],
-                                            info["input_balance_0"], info["output_balance_0"],
-                                            info["swap_rate"], info["market_rate"],
-                                            "MCSMM"))
-
-                for t in self.balances:
-                    pool_dict[t] = (self.balances[t], 0.5)
-                batch_status.append(MultiTokenPoolStatus(pool_dict))
-
-                if self.reset_tx:
-                    self.balances = balance_copy
-                    balance_copy = deepcopy(self.balances)
-
-            outputs.append(batch_output)
-            status.append(batch_status)
-
-        for t in self.balances:
-            pool_dict[t] = (self.balances[t], 0.5)
-
-        return outputs, status, initial_copy, MultiTokenPoolStatus(pool_dict), rates, rates
+        return output_tx, pool_stat
