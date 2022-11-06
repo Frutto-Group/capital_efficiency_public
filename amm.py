981707c21e4cfee27be1ed54bf63251e34a8dc1a
WayneChen2021 <91292224+WayneChen2021@users.noreply.github.com>
2022-11-06 00:04:11 -0400

    bug fixes

--- amm.py
+++ amm.py
@@ -1,6 +1,6 @@
 from imarketmaker import MarketMakerInterface
 from inputtx import InputTx
-from typing import List, Tuple, str, float
+from typing import List, Tuple
 from outputtx import OutputTx
 from poolstatus import PairwiseTokenPoolStatus
 
@@ -37,16 +37,15 @@ class AMM(MarketMakerInterface):
         1. output information associated with swap (after_rate is incorrect)
         2. status of pool ater swap
         """
-        if out_amt == None:
-            in_type, in_val, out_type = tx.intype, tx.inval, tx.outtype
-            in_balance, out_balance, k = self.token_info[(in_type, out_type)]
-            const = in_balance * out_balance
-            out_amt = const*(1/in_balance - (1/(in_balance + in_val)))
+        in_type, in_val, out_type = tx.intype, tx.inval, tx.outtype
+        in_balance, out_balance, k = self.token_info[(in_type, out_type)]
+        const = in_balance * out_balance
+        out_amt = const*(1/in_balance - (1/(in_balance + in_val)))
         
         output_tx, pool_stat = super().swap(tx, out_amt)
         output_tx.after_rate = in_val / \
                 (const*(1/(in_balance + in_val) - (1/(in_balance + 2*in_val))))
-        
+
         return output_tx, pool_stat
     
     def calculate_equilibriums(self, intype: str, outtype: str) -> Tuple[float, float]:
@@ -63,7 +62,7 @@ class AMM(MarketMakerInterface):
         """
         pool = (intype, outtype)
         const = self.token_info[pool][1] * self.token_info[pool][0]
-        market_rate = self.external_price[pool[1]] / self.external_price[pool[0]]
+        market_rate = self.prices[pool[1]] / self.prices[pool[0]]
         new_out = (const / market_rate) ** 0.5
 
         return const / new_out, new_out
