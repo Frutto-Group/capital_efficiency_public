33bf878cfa7c66b5db7aa251edf1471e58616597
WayneChen2021 <91292224+WayneChen2021@users.noreply.github.com>
2022-11-06 23:41:54 -0500

    Update pmm.py

--- pmm.py
+++ pmm.py
@@ -6,23 +6,18 @@ from copy import deepcopy
 from poolstatus import PairwiseTokenPoolStatus
 
 class PMM(MarketMakerInterface):
-    def __init__(self, token_pairs: List[Tuple[str]], token_amounts: List[Tuple[float]]):
+    def __init__(self, pairwise_pools: List[Tuple[str, str]],
+    pairwise_infos: List[Tuple[float, float, float]], single_pools = None, single_infos = None):
         """
-        Creates a proactive market maker with pairwise liquidity pool
+        Creates a constant product pairwise liquidity pool market maker
 
         Parameters:
-        1. token_pairs: specifies pairwise liquidity pools; of the form:
-        [["BTC", "ETH"],
-         ["ETH", "BTC"],
-         ["BTC", "USDT"],
-         ["USDT", "BTC"]]
-        2. token_info: specifies liquidity pool starting token balances; of the form:
-        [[1100, 500, 0],
-         [500, 1100, 0],
-         [200, 500, 0],
-         [500, 200, 0]]
+        1. pairwise_pools: specifies pairwise liquidity pools
+        2. pairwise_infos: specifies liquidity pool starting token balances
+        3. single_pools: irrelevant (for multi token market makers)
+        4. single_infos: irrelevant (for multi token market makers)
         """
-        self.token_info = PairwiseTokenPoolStatus(token_pairs, token_amounts)
+        self.token_info = PairwiseTokenPoolStatus(pairwise_pools, pairwise_infos)
         self.equilibriums = deepcopy(self.token_info)
 
     def __solveLong(self, x: float, l_e: float, s_e: float, p: float, k: float
@@ -36,6 +31,10 @@ class PMM(MarketMakerInterface):
         2. s_e: shortage token equilibrium balance
         3. p: exchange rates in units of excess tokens / shortage tokens
         4. k: k parameter between 2 token types
+
+
+        Returns:
+        1. excess token balance
         """
         return l_e - p * (x - s_e) * (1 - k + k * s_e / x)
     
@@ -50,6 +49,9 @@ class PMM(MarketMakerInterface):
         2. L: excess token equilibrium balance
         3. p: exchange rates in units of excess tokens / shortage tokens
         4. k: k parameter between 2 token types
+
+        Returns:
+        1. shortage token balance
         """
         return (y-L-p*S+2*k*p*S-(y**2-2*y*L+L**2-2*y*p*S+4*k*y*p*S+2*L*p*S-4*k*L*p*S+p**2*S**2)**0.5)\
             /(2*(-1+k)*p)
@@ -70,33 +72,37 @@ class PMM(MarketMakerInterface):
         2. status of pool ater swap
         """
         pool = (tx.intype, tx.outtype)
+        k = self.token_info[pool][2]
+        i_0 = self.token_info[pool][0]
+        o_0 = self.token_info[pool][1]
+        in_e, out_e = self.calculate_equilibriums(tx.intype, tx.outtype)
+        
         if out_amt == None:
             d = tx.inval
-            k = self.token_info[pool][2]
             p = self.prices[tx.outtype] / self.prices[tx.intype]
-            in_e, out_e = self.calculate_equilibriums(tx.intype, tx.outtype)
-
-            i_0 = self.token_info[pool][0]
-            o_0 = self.token_info[pool][1]
 
             if o_0 / out_e > i_0 / in_e:
                 s_e, l_e = in_e, out_e
                 static_amt = s_e - i_0
 
                 if static_amt < d:
-                    out_amt = self.__solveShort(d - static_amt + s_e, s_e, l_e, p, k)
+                    new_pt = self.__solveShort(d - static_amt + s_e, s_e, l_e, p, k)
                 else:
-                    out_amt = self.__solveLong(i_0 + d, l_e, s_e, 1/p, k)
+                    new_pt = self.__solveLong(i_0 + d, l_e, s_e, 1/p, k)
             else:
                 s_e, l_e = out_e, in_e
-                out_amt = self.__solveShort(i_0 + d, l_e, s_e, p, k)
+                new_pt = self.__solveShort(i_0 + d, l_e, s_e, p, k)
         
-        output_tx, pool_stat = super().swap(tx, out_amt, execute)
+        if out_amt == None:
+            output_tx, pool_stat = super().swap(tx, o_0 - new_pt, execute)
+        else:
+            output_tx, pool_stat = super().swap(tx, out_amt, execute)
+        
         if execute:
-            self.equilibriums[pool] = (in_e, out_e)
-            self.equilibriums[(tx.outtype, tx.intype)] = (out_e, in_e)
+            self.equilibriums[pool] = [in_e, out_e, k]
+            self.equilibriums[(tx.outtype, tx.intype)] = [out_e, in_e, k]
 
-            o, _ = self.swap(tx, out_amt, False)
+            o, _ = self.swap(tx, None, False)
             output_tx.after_rate = tx.inval / \
                 (o.outpool_init_val - o.outpool_after_val)
         
@@ -111,8 +117,8 @@ class PMM(MarketMakerInterface):
         2. outtype: output token type
 
         Returns:
-        1. Equilibrium balance for input token
-        2. Equilibrium balance for output token
+        1. equilibrium balance for input token
+        2. equilibrium balance for output token
         """
         firstIsLong = True
         pool = (intype, outtype)
