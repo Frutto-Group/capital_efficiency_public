1be1ef831cd3ae6552324b1e5aaf0d7566885a20
WayneChen2021 <91292224+WayneChen2021@users.noreply.github.com>
2022-11-16 20:47:56 -0500

    Update inputtx.py

--- inputtx.py
+++ inputtx.py
@@ -1,33 +1,16 @@
-from typing import str, float, bool
-
 class InputTx:
-    def __init__(self, input_token_type : str, output_token_type : str, input_token_value : float, is_arb : bool = False):
+    def __init__(self, intype : str, outtype : str, inval : float, is_arb : bool = False):
         """
         Represents one transaction to in traffic
 
         Parameters:
-        input_token_type: input token name
-        output_token_type: output token name
-        input_token_value: amount of input token to be inserted in market maker's liquidity pool
-        is_arb: whether or not this transaction is processed or will result in call to arbitrage() in market maker
+        intype: input token name
+        outtype: output token name
+        inval: amount of input token to be inserted in market maker's liquidity pool
+        is_arb: whether or not this transaction is processed or will result in
+        call to arbitrage() in market maker
         """
-        self.input_token_type  = input_token_type
-        self.output_token_type = output_token_type
-        self.input_token_value = input_token_value
+        self.intype  = intype
+        self.outtype = outtype
+        self.inval = inval
         self.is_arb = is_arb
-
-    @property
-    def id(self) -> int:
-        return self.index
-
-    @property
-    def intype(self) -> str:
-        return self.input_token_type
-
-    @property
-    def outtype(self) -> str:
-        return self.output_token_type
-
-    @property
-    def inval(self) -> float:
-        return self.input_token_value
