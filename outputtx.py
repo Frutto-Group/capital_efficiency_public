72edbd1ae891353e75e7e48e2924f970bd1aaafc
WayneChen2021 <91292224+WayneChen2021@users.noreply.github.com>
2022-11-16 20:50:09 -0500

    Update outputtx.py

--- outputtx.py
+++ outputtx.py
@@ -1,9 +1,8 @@
-from typing import str, float
 
 class OutputTx:
     def __init__(self,
-                 input_token_type: str,
-                 output_token_type: str,
+                 in_type: str,
+                 out_type: str,
                  inpool_init_val: float,
                  outpool_init_val: float,
                  inpool_after_val: float,
@@ -15,8 +14,8 @@ class OutputTx:
         Information associated with each swap for computing metrics
 
         Parameters:
-        input_token_type: input token name
-        output_token_type: output token name
+        in_type: input token name
+        out_type: output token name
         inpool_init_val: amount of input token originally in market maker's liquidiy pool
         outpool_init_val: amount of output token originally in market maker's liquidiy pool
         inpool_after_val: amount of input token in market maker's liquidiy pool after swap
@@ -24,8 +23,8 @@ class OutputTx:
         market_rate: exchange rate in market outside market maker
         after_rate: exchange rate inside market maker of swap types after swap occurs
         """
-        self.input_token_type = input_token_type
-        self.output_token_type = output_token_type
+        self.in_type = in_type
+        self.out_type = out_type
         self.inpool_init_val = inpool_init_val
         self.outpool_init_val = outpool_init_val
         self.inpool_after_val = inpool_after_val
